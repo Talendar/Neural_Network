efff2833c3f27fcae96d5d721a012b5fbf4fb19b
Gabriel Nogueira <51515680+Talendar@users.noreply.github.com>
2019-08-11 14:50:21 -0300

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -130,13 +130,6 @@ class NeuralNetwork:
 
         raise NameError("Cost function of the type \"%s\" is not defined!" % str(self.cost_type))
 
-        # non-vectorized general implementation
-        """total = 0
-        for x, y in zip(training_set, labels):
-            x, y = self.colvector(x), self.colvector(y)
-            total += sum(self.costfunc_unit(self.predict(x), y))
-        return total / len(training_set)"""
-
 
     def colvector(self, v):
         """
@@ -210,6 +203,48 @@ class NeuralNetwork:
         return grad_w, grad_b, activations[-1]
 
 
+    def params_to_row(self, weights, bias):
+        """
+
+        :param weights:
+        :param bias:
+        :return:
+        """
+        flat_params = np.concatenate((weights[0].flatten(), bias[0].flatten()))
+        for w, b in zip(weights[1:], bias[1:]):
+            temp = np.concatenate((w.flatten(), b.flatten()))
+            flat_params = np.concatenate((flat_params, temp))
+
+        return flat_params
+
+
+    def params_to_matrix(self, flat_params):
+        """
+
+        :param flat_params:
+        :return:
+        """
+        weights, bias = [], []
+        consumed = 0
+
+        for l in self.layers[1:]:
+            rows_w, cols_w = l.weights.shape
+            total_w = rows_w * cols_w
+            w = flat_params[consumed : consumed + total_w]
+            w.shape = (rows_w, cols_w)
+            weights.append(w)
+            consumed += total_w
+
+            rows_b, cols_b = l.bias.shape
+            total_b = rows_b * cols_b
+            b = flat_params[consumed: consumed + total_b]
+            b.shape = (rows_b, cols_b)
+            bias.append(b)
+            consumed += total_b
+
+        return weights, bias
+
+
     def compute_gradient_numerically(self, data_x, data_y):
         """
         Compute the gradient of the cost function numerically. To be used in gradient checking only.
@@ -218,47 +253,41 @@ class NeuralNetwork:
         :param data_y:
         :return:
         """
-        eps = 1e-5
+        e = 1e-4
+        flat_params = self.params_to_row( [l.weights for l in self.layers[1:]] , [l.bias for l in self.layers[1:]] )
+        num_grad = np.zeros(flat_params.shape)
+        pertub = np.zeros(flat_params.shape)
 
-        base_weights = [l.weights for l in self.layers[1:]]
-        base_bias = [l.bias for l in self.layers[1:]]
+        for i in range(0, len(flat_params)):
+            pertub[i] = e
+            inc_params = self.params_to_matrix(flat_params + pertub)
+            dec_params = self.params_to_matrix(flat_params - pertub)
 
-        grad_w = [np.zeros(l.weights.shape) for l in self.layers[1:]]
-        grad_b = [np.zeros(l.bias.shape) for l in self.layers[1:]]
+            inc_predictions = [(self.feedforward(*inc_params, x), y) for x, y in zip(data_x, data_y)]
+            dec_predictions = [(self.feedforward(*dec_params, x), y) for x, y in zip(data_x, data_y)]
 
-        for l in range(0, len(self.layers) - 1):
-            for row in range(len(base_weights[l])):
+            J = self.costfunc
+            num_grad[i] += (J(inc_predictions) - J(dec_predictions)) / (2*e)
+            pertub[i] = 0
 
-                # WEIGHTS
-                for col in range(len(base_weights[l][row])):
-                    temp_weights_plus = [w.copy() for w in base_weights]
-                    temp_weights_minus = [w.copy() for w in base_weights]
+        return num_grad
 
-                    temp_weights_plus[l][row][col] += eps
-                    temp_weights_minus[l][row][col] -= eps
 
-                    predictions_plus = [( self.feedforward(temp_weights_plus, base_bias, x), y )
-                                        for x, y in zip(data_x, data_y)]
-                    predictions_minus = [( self.feedforward(temp_weights_minus, base_bias, x), y )
-                                        for x, y in zip(data_x, data_y)]
-                    grad_w[l][row][col] = (self.costfunc(predictions_plus) - self.costfunc(predictions_minus)) / (2*eps)
-                    #print("W[%d][%d][%d] = %f" % (l, row, col, grad_w[l][row][col]))
+    def gradient_checking(self, bp_grad_w, bp_grad_b, data_x, data_y):
+        """
 
-                # BIAS
-                temp_bias_plus = [b.copy() for b in base_bias]
-                temp_bias_minus = [b.copy() for b in base_bias]
+        :param bp_grad_w:
+        :param bp_grad_b:
+        :return:
+        """
+        bp_grad = self.params_to_row(bp_grad_w, bp_grad_b)
+        num_grad = self.compute_gradient_numerically(data_x, data_y)
 
-                temp_bias_plus[l][row] += eps
-                temp_bias_minus[l][row] -= eps
+        relative_error = np.linalg.norm(bp_grad - num_grad) / np.linalg.norm(bp_grad + num_grad)
 
-                predictions_plus = [( self.feedforward(base_weights, temp_bias_plus, x), y )
-                                    for x, y in zip(data_x, data_y)]
-                predictions_minus = [( self.feedforward(base_weights, temp_bias_minus, x), y )
-                                     for x, y in zip(data_x, data_y)]
-                grad_b[l][row] = (self.costfunc(predictions_plus) - self.costfunc(predictions_minus)) / (2*eps)
-                #print("\nB[%d][%d] = %f\n" % (l, row, grad_b[l][row]))
+        with open(self.logs_path + "/log_gradient_checking.txt", "a") as file:
+            file.write("Relative error: %.4e\n\n" % relative_error)
 
-        return grad_w, grad_b
 
 
     def regularization_term(self, m, method, factor, derivative=False, w=None):
@@ -297,15 +326,14 @@ class NeuralNetwork:
         :param mini_batch_size: size of the mini batches of samples to be used in each iteration.
         :return:
         """
-        # clearing log files
+        # Prep
         if gradient_checking:
             with open(self.logs_path + "/log_gradient_checking.txt", "w") as file:
                 file.write("< Started at: " + str(datetime.datetime.now()) + " >\n\n")
-
-        # SGD
         starting_time = time.time()
         all_costs = []
 
+        # SGD
         for e in range(epochs):
             mini_batches = self.generate_mini_batches(training_data, labels, mini_batch_size)
             predictions = []
@@ -327,6 +355,10 @@ class NeuralNetwork:
                 grad_w = [w/m for w in grad_w]
                 grad_b = [b/m for b in grad_b]
 
+                # gradient checking (this won't work if you grad check after updating the params!)
+                if gradient_checking:
+                    self.gradient_checking(grad_w, grad_b, mini_batch_x, mini_batch_y)
+
                 # updating weights and bias
                 for i, l in enumerate(self.layers[1:]):
                     gw, gb = grad_w[i], grad_b[i]
@@ -336,23 +368,7 @@ class NeuralNetwork:
                                                                     reg_factor, derivative=True, w=l.weights)
                     l.weights -= learning_rate * (gw + reg_term)
 
-                # gradient checking (log results)
-                if gradient_checking:
-                    num_grad_w, num_grad_b = self.compute_gradient_numerically(mini_batch_x, mini_batch_y)
-
-                    flat_num_grad = np.concatenate((num_grad_w[0].flatten(), num_grad_b[0].flatten()))
-                    flat_grad = np.concatenate((grad_w[0].flatten(), grad_b[0].flatten()))
-                    for l in range(1, len(self.layers) - 1):
-                        flat_num_grad = np.concatenate((flat_num_grad,
-                                                         np.concatenate((num_grad_w[l].flatten(), num_grad_b[l].flatten()))))
-                        flat_grad = np.concatenate((flat_grad,
-                                                    np.concatenate((grad_w[l].flatten(), grad_b[l].flatten()))))
-
-                    relative_error = np.linalg.norm(flat_grad - flat_num_grad) / np.linalg.norm(flat_grad + flat_num_grad)
-                    with open(self.logs_path + "/log_gradient_checking.txt", "a") as file:
-                        file.write("Relative error: %f\n\n" % relative_error)
-
-            # cost function
+            # cost function history
             all_costs.append(self.costfunc(predictions))
 
             # print status
@@ -377,6 +393,7 @@ class NeuralNetwork:
                       % ((e + 1), epochs, bar, h, m, s, all_costs[-1]))
 
         return all_costs
+
 
     def save_params(self, file_name="./data/params.txt"):
         """
@@ -420,6 +437,7 @@ class NeuralNetwork:
         file.close()
 
 
+
 class NeuralLayer:
     """
     """
@@ -454,3 +472,41 @@ class NeuralLayer:
             return z if not derivative else 1
 
         raise NameError("Activation function of the type \"%s\" is not defined!" % str(self.activation))
+
+
+
+def gradient_checking_sample():
+    """
+
+    :return:
+    """
+    m = 32
+    net = NeuralNetwork([1, 10, 10, 10, 1], layers_activation="sigmoid")
+    net.layers[-1].activation = "linear"
+
+    training_data = np.array([x for x in np.random.rand(m)*100])
+    training_labels = np.array([2*x for x in training_data])
+
+    # BACKPROP
+    bp_grad_w = [np.zeros(l.weights.shape) for l in net.layers[1:]]
+    bp_grad_b = [np.zeros(l.bias.shape) for l in net.layers[1:]]
+    for x, y in zip(training_data, training_labels):
+        x, y = net.colvector(x), net.colvector(y)
+        gw_var, gb_var, h = net.backpropagation(x, y)
+
+        bp_grad_w = [cur + var for cur, var in zip(bp_grad_w, gw_var)]
+        bp_grad_b = [cur + var for cur, var in zip(bp_grad_b, gb_var)]
+
+    bp_grad_w = [w / m for w in bp_grad_w]
+    bp_grad_b = [b / m for b in bp_grad_b]
+    bp_grad = net.params_to_row(bp_grad_w, bp_grad_b)
+
+    # NUM GRAD
+    num_grad = net.compute_gradient_numerically(training_data, training_labels)
+
+    # COMPARE
+    for bp, num in zip(bp_grad, num_grad):
+        print("%f  |  %f" % (bp, num))
+
+    relative_error = np.linalg.norm(bp_grad - num_grad) / np.linalg.norm(bp_grad + num_grad)
+    print("\nRelative error: %.2e" % relative_error)
